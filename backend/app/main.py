dc8b181939678dda5b7059d1d939d4c3208eb4f9
shuji19960425 <shuji0425sign@gmail.com>
2025-06-01 22:02:14 +0900

    fastAPIの設定完了

---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from app.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# CORS設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 本番では制限すること
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ルート
+app.include_router(router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Voice Login API is running."}
